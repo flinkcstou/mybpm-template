34e813a494fd6910c0f88398e1ca5d4a7284d678
nabu <nabu@greet-go.com>
2023-09-13 14:09:42 +0600

    Initial commit

---
+++ apps/mybpm-template-application/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "mybpm-template-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
+})
+export class AppComponent {
+  title = "mybpm-template-application";
+}
